ff58b74d0a0fd9014609b1d7e9e483d9d8b90027
Bitzark 11 <dayronsotelo224@unisangil.edu.co>
2025-02-26 14:41:47 -0500

    Tipos de variables 1.0

---
+++ variables.py
@@ -0,0 +1,15 @@
+entero = 100
+print (type(entero))
+decimal = 10.3
+print (type(decimal))
+letra = 'a'
+print (type(letra))
+mensaje = "Hola mundo"
+print (type(mensaje))
+estado = False
+print (type(estado))
+parrafo = """
+print ('Hola mundo')
+print ('Championship')
+"""
+print (type(parrafo))
