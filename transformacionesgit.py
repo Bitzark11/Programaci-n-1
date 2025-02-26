86de73e438aade73ad771db9f37f6ed4d0efcc3e
Bitzark 11 <dayronsotelo224@unisangil.edu.co>
2025-02-26 14:59:15 -0500

    Transformaciones de variables 1.0

---
+++ transformacionesgit.py
@@ -0,0 +1,14 @@
+nombre = input("Ingrese su nombre")
+print (nombre)
+print(type(nombre))
+#Edad
+edad = int (input("Ingrese su edad"))
+print (edad)
+print(type(edad))
+#Estado de matricula
+estado = bool(input("Estado de matricula activo/inactivo"))
+print(estado)
+print (type(estado))
+#Peso
+peso = float(input("Ingrese su peso: "))
+print (type(peso))
