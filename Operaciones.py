eb6ee9b3db1a45372d1d01986de527f0a4272c4b
Bitzark 11 <dayronsotelo224@unisangil.edu.co>
2025-03-05 14:32:59 -0500

    Operaciones V1.0

---
+++ Operaciones.py
@@ -0,0 +1,10 @@
+import math
+ejemplo = 10+(2*(-3*((7-2)+7*(3-2))))
+print (ejemplo)
+
+
+ejemplo2 = 2*(4*(7+4*(3*5-9))-3*(40-8)) 
+print (ejemplo2)
+
+ejemplo3 = 3*(4+(2*(3+5)))+2
+print (ejemplo3) 
