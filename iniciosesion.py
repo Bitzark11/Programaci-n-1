4148d03b5ef46f6b46627c8d8f3d043578855c71
Bitzark 11 <dayronsotelo224@unisangil.edu.co>
2025-03-05 16:12:54 -0500

    operadores logicos V1.0

---
+++ iniciosesion.py
@@ -0,0 +1,9 @@
+dayron = "dsotelom"
+password = 1234
+
+#Solicitud de datos
+usuario = input ("Usuario: ")
+contraseña = int (input("Contraseña: "))
+
+#Validacion
+print(dayron == usuario and password == contraseña)
---
+++ operadoreslogicos.py
@@ -0,0 +1,17 @@
+num1 = 5
+num2 = 5
+num3 = 10
+
+
+print (num1 == num2 and num1 != num3)
+print (num1 == num2 and num1 > num3)
+print (num1 != num2 and num1 > num3)
+print (num1 != num2 and num1 < num3)
+
+print (num1 == num2 or num1 != num3)
+print (num1 == num2 or num1 > num3)
+print (num1 != num2 or num1 > num3)
+print (num1 != num2 or num1 < num3)
+
+print (not num1 == num2)
+print (not num1 != num2)
